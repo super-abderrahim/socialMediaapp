b47732faedc41d643bd8e816fc353e7214fcaf0c
super-abderrahim <148150340+super-abderrahim@users.noreply.github.com>
2024-10-19 01:14:24 +0100

    Fix client folder

--- client1/src/scenes/widgets/FriendListWidget.jsx
+++ client1/src/scenes/widgets/FriendListWidget.jsx
@@ -13,7 +13,7 @@ const FriendListWidget = ({ userId }) => {
 
   const getFriends = async () => {
     const response = await fetch(
-      `http://localhost:3001/users/${userId}/friends`,
+      `https://socialmediaapp-server-52iy.onrender.com/users/${userId}/friends`,
       {
         method: "GET",
         headers: { Authorization: `Bearer ${token}` },
